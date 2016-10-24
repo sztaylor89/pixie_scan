5bcc18bf150df72f2042b405f79780a27a4327d5
Steven Taylor <sztaylor89@gmail.com>
2016-10-24 19:35:23 -0400

    more scripts added

---
+++ SCANSTUFF.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+name="bbb"
+data="/home/steve/Desktop/local_scan/ldf"
+
+rm -f $name.his $name.dat $name.drr $name.list $name.log
+
+for i in `ls -tr $data/a135feb_12.ldf`
+#for i in `ls -tr $data/a135feb_12*.ldf`
+do
+    #if [ "$i" == "$data/a135feb_12.ldf" ];
+    #then
+    #continue
+    #fi
+
+
+    cmd=$cmd"file $i\ngo\n"
+
+done
+cmd=$cmd"end\n"
+#echo -e $cmd
+echo -e $cmd | ./pixie_ldf_c $name
